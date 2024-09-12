e29d58d713cc211a46d8b2eb14966d58b98dae5a
Aleksey-Markov <tte4enuxa@gmail.com>
2024-09-12 19:51:09 +0700

    add docker-compose

--- materials/validators.py
+++ materials/validators.py
@@ -6,14 +6,14 @@ class LinkValidator:
     def __init__(self, field):
         self.field = field
 
-    def __call__(self, value):
-        reg = re.compile('\Byoutube\B')
-
-        tmp_val = dict(value).get(self.field)
-        if not tmp_val:
-            return
-
-        first_match = re.search('youtube.com', tmp_val)
-
-        if not first_match:
-            raise ValidationError(f"Ошибка! Допустимы ссылки только на YouTube.")
+    # def __call__(self, value):
+    #     reg = re.compile('\Byoutube\B')
+    #
+    #     tmp_val = dict(value).get(self.field)
+    #     if not tmp_val:
+    #         return
+    #
+    #     first_match = re.search('youtube.com', tmp_val)
+    #
+    #     if not first_match:
+    #         raise ValidationError(f"Ошибка! Допустимы ссылки только на YouTube.")
